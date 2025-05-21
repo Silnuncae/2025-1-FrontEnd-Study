1998eda4c1b21f1636638e96147117917abf67ae
silnuncae <code.stingmon@gmail.com>
2025-05-21 18:32:52 +0900

    Update: 모든 파일 수정

---
+++ week5/script.js
@@ -0,0 +1,26 @@
+let root = document.getElementById("root");
+let button = document.getElementById("call");
+
+function render(data) {
+    for (let i = 0; i < data.length; i++) {
+        console.log(data);
+        let h2Tag = document.createElement("h2");
+        h2Tag.innerText = data[i].title;
+        if (data[i].completed) {
+            h2Tag.classList.add("line");
+        }
+        root.appendChild(h2Tag);
+    }
+    button.removeEventListener("click", onButtonClick);
+    button.textContent = "완료!";
+    button.setAttribute("disabled", true);
+};
+
+function onButtonClick() {
+    console.log("버튼 클릭");
+    fetch('http://jsonplaceholder.typicode.com/users1/1/todos')
+        .then((response) => response.json())
+        .then((json) => render(json));
+}
+
+button.addEventListener("click", onButtonClick);
